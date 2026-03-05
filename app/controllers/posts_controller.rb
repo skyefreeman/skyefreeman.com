1bd7deddbf959abeecce66c3df7d267b926d12b5
skye <skyefreeman@icloud.com>
2026-03-04 23:38:48 -0500

    add create, edit functionality to posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,52 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(
+      :title, :author, :state, :deleted_at,
+      :body, :header_attachment, body_attachments: []
+    )
   end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -10,4 +10,29 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     get post_path(posts(:one))
     assert_response :success
   end
+
+  test "new returns a blank post form" do
+    get new_post_path
+    assert_response :success
+  end
+
+  test "create saves a new post and redirects" do
+    post posts_path, params: { post: { title: "New Post", author: "Skye", state: "draft", body: "Hello" } }
+    assert_redirected_to post_path(Post.last)
+  end
+
+  test "edit returns the post form" do
+    get edit_post_path(posts(:one))
+    assert_response :success
+  end
+
+  test "update saves changes and redirects" do
+    patch post_path(posts(:one)), params: { post: { title: "Updated Title" } }
+    assert_redirected_to post_path(posts(:one))
+  end
+
+  test "destroy deletes the post and redirects to index" do
+    delete post_path(posts(:one))
+    assert_redirected_to posts_path
+  end
 end
