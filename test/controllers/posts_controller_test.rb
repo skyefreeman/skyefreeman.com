587f69a4e225e1547b34e11c628c5abd17837ef1
skye <skyefreeman@icloud.com>
2026-03-04 23:20:17 -0500

    post index, show

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index returns all posts" do
+    get posts_path
+    assert_response :success
+  end
+
+  test "show returns a single post" do
+    get post_path(posts(:one))
+    assert_response :success
+  end
 end
