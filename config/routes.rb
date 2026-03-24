d027873737e0a18dcd7426cbc7a3f87139029dea
skye <skyefreeman@icloud.com>
2026-03-23 21:53:25 -0400

    lint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  resources :posts, except: [:index]
+  resources :posts, except: [ :index ]
 
   get "blog", to: "posts#blog"
   get "blog/new", to: "posts#new"
