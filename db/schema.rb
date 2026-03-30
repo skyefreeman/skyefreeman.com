1dbeaffddfdba27fee4468765e70d38a19269abd
skye <skyefreeman@icloud.com>
2026-03-29 23:16:35 -0400

    migrate links.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_24_062020) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_30_021327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_24_062020) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "posts", force: :cascade do |t|
